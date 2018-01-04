fd9ca27ebc70f36fb3af6c5e9fd932f472540231
maximerischard <rischardmaxime@gmail.com>
2018-01-04 13:07:51 +0000

    update constant kernel

--- Maxime/src/constant_kernel.jl
+++ Maxime/src/constant_kernel.jl
@@ -1,7 +1,7 @@
-using GaussianProcesses: Stationary, MatF64, VecF64, StationaryData, EmptyData
-import GaussianProcesses: cov, cov!, addcov!, kernel_data_key, get_params, set_params!, get_param_names, num_params
+using GaussianProcesses: Isotropic, MatF64, VecF64, SqEuclidean
+import GaussianProcesses: cov, cov!, addcov!, kernel_data_key, get_params, set_params!, get_param_names, num_params, dk_dθp, dk_dlσ
 
-type ConstantKernel <: Stationary
+type ConstantKernel <: Isotropic{SqEuclidean}
     σ2::Float64
     priors::Array
     ConstantKernel(lσ::Float64) = new(exp(2*lσ), [])
@@ -14,7 +14,7 @@ get_param_names(::ConstantKernel) = [:lσ]
 num_params(k::ConstantKernel) = 1
 kernel_data_key{M<:MatF64}(k::ConstantKernel, X::M) = "ConstData"
 cov(k::ConstantKernel, r::Float64) = k.σ2
-cov{V1<:VecF64,V2<:VecF64}(k::ConstantKernel, x::V1, y::V2) = k.σ2
+# cov{V1<:VecF64,V2<:VecF64}(k::ConstantKernel, x::V1, y::V2) = k.σ2
 
 @inline function dk_dθp(k::ConstantKernel, r::Float64, p::Int)
     if p==1
@@ -23,12 +23,3 @@ cov{V1<:VecF64,V2<:VecF64}(k::ConstantKernel, x::V1, y::V2) = k.σ2
         return NaN
     end
 end
-# distance{M<:MatF64}(k::ConstantKernel, X::M) = ones(Float64, size(X,2), size(X,2))
-# distance{M1<:MatF64,M2<:MatF64}(k::ConstantKernel, X::M1, Y::M2) = ones(Float64, size(X,2), size(Y,2))
-# distance{V1<:VecF64,V2<:VecF64}(k::ConstantKernel, x::V1, y::V2) = 1.0
-function cov!{M<:MatF64}(cK::MatF64, k::ConstantKernel, X::M, data::EmptyData)
-    fill!(cK, k.σ2)
-end
-function addcov!{M<:MatF64}(cK::MatF64, k::ConstantKernel, X::M, data::EmptyData)
-    cK[:,:] += k.σ2
-end   
